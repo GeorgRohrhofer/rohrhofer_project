9d48cbb0ef976ae504686e5eff00403022278854
Georg Rohrhofer <rohrhofer.georg@student.htlwrn.ac.at>
2023-03-16 10:40:14 +0100

    add protobuf message

--- json-rpc-client/src/main.cpp
+++ json-rpc-client/src/main.cpp
@@ -1,11 +1,15 @@
 
 #include <iostream>
 #include <thread>
+#include <asio.hpp>
+
+#include "messages.pb.h"
 
 using namespace std;
 
 //int main(int argc, char* argv[]) {
 int main() {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
     thread t{[]{ cout << "Hello"; }};
     t.join();
     cout << " world!" << endl;
--- json-rpc-server/src/main.cpp
+++ json-rpc-server/src/main.cpp
@@ -1,13 +1,22 @@
 
 #include <iostream>
 #include <thread>
+#include <asio.hpp>
+
+#include "messages.pb.h"
 
 using namespace std;
 
 //int main(int argc, char* argv[]) {
 int main() {
-    thread t{[]{ cout << "Hello"; }};
-    t.join();
-    cout << " world!" << endl;
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    JsonRPC *jrpc = new JsonRPC;
+    jrpc->set_text("Hallo");
+
+    cout << jrpc->text() << endl;
+    jrpc->SerializeToOstream(&cout);
+
+    jrpc->ParseFromIstream(&cin);
+    delete jrpc;
     return 0;
 }
