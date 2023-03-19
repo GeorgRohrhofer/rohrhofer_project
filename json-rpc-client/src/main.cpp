66aba2c0347b2f2c4937d60353553c09ebeb94b3
Georg Rohrhofer <rohrhofer.georg@student.htlwrn.ac.at>
2023-03-19 16:34:09 +0100

    Add basic communication with protobuf

--- json-rpc-client/src/main.cpp
+++ json-rpc-client/src/main.cpp
@@ -1,17 +1,27 @@
 
 #include <iostream>
+#include <ostream>
 #include <thread>
 #include <asio.hpp>
 
 #include "messages.pb.h"
 
 using namespace std;
+using namespace asio::ip;
 
 //int main(int argc, char* argv[]) {
 int main() {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
-    thread t{[]{ cout << "Hello"; }};
-    t.join();
-    cout << " world!" << endl;
+    JsonRPC *jrpc = new JsonRPC();
+    jrpc->set_text("HELO");
+    tcp::iostream *strm = new tcp::iostream("localhost", "9999");
+    //ostream str{};
+    if(strm){
+        jrpc->SerializeToOstream(strm);
+        //strm << str;
+    }
+
+    delete jrpc;
+    delete strm;
     return 0;
 }
--- json-rpc-server/src/main.cpp
+++ json-rpc-server/src/main.cpp
@@ -6,17 +6,32 @@
 #include "messages.pb.h"
 
 using namespace std;
+using namespace asio::ip;
 
 //int main(int argc, char* argv[]) {
 int main() {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     JsonRPC *jrpc = new JsonRPC;
-    jrpc->set_text("Hallo");
+    //jrpc->set_text("Hallo")
+    asio::io_context ctx;
+    tcp::endpoint ep{tcp::v4(), 9999};
+    tcp::acceptor acceptor{ctx, ep}; // IO object
+    acceptor.listen();
+    tcp::socket sock{ctx};
+    acceptor.accept(sock);
+    tcp::iostream *strm = new tcp::iostream(std::move(sock));
+    //shorter: tcp::iostream strm{acceptor.accept()};
+    jrpc->ParseFromIstream(strm);
+    strm->close();
 
+    cout << jrpc->text() << endl;
+
+    /*
     cout << jrpc->text() << endl;
     jrpc->SerializeToOstream(&cout);
 
     jrpc->ParseFromIstream(&cin);
+    */
     delete jrpc;
     return 0;
 }
